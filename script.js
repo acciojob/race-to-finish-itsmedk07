837bffe0950c42e049a7d89a8c792338fa1e1a49
itsmedk07 <126004945+itsmedk07@users.noreply.github.com>
2023-07-21 04:42:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const prom=new promise((resolve,reject)=>{
+	console.log(resolve("hiiii"))
+})
+promise.any(prom)
